4be87fc24d083b198afb2a08635ad51e3cb288dd
Mikhail Khludnev <mkhludnev@griddynaics.com>
2012-08-03 00:28:05 +0400

    revamp generator to parallel files

--- join-tester/src/main/java/joins/queries/QueryGenerator.java
+++ join-tester/src/main/java/joins/queries/QueryGenerator.java
@@ -3,18 +3,84 @@ package joins.queries;
 import joins.indexer.WordLists;
 
 import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class QueryGenerator {
   private final String OUTPUT_FILE = "queries.txt";
   private static final int ACL_RANGE = 10000;
 
-
-  public static void main(String[] args) {
-	  QueryGenerator qg = new QueryGenerator();
-    main(qg);
+  protected QGen queryConsumer;
+  
+  interface QGen {
+	  void onQuery(String terms, String aclRange);
   }
+  
+  static class QueryFile implements QGen, Closeable{
+	  
+	  private final BufferedWriter writer;
+	private String format;
+	  
+	  QueryFile(String fileName, String format) throws IOException{
+		  writer = new BufferedWriter(new FileWriter(fileName));
+		  this.format = format;
+	  }
+	  
+	@Override
+	public void close() throws IOException {
+		writer.close();
+	}
+
+	@Override
+	public void onQuery(String terms, String aclRange) {
+	    try {
+			writer.write(String.format(format,
+			        terms, aclRange ));
+			writer.newLine();
+		} catch (IOException e) {
+			throw new RuntimeException("wrap", e);
+		}
+	}
+	  
+  }
+
+	public static void main(String[] args) throws IOException {
+		final List<QueryFile> consumers = Arrays
+				.asList(new QueryFile("queries-join.txt", "q=text_all:(%s)" +
+				// "&fl=id,score&sort=score desc&fq={!join from=few_join_id to=few_id}acl:%s",
+						"&fl=id,score&sort=score desc&fq={!join from=join_id to=id}acl:%s"),
+						new QueryFile(
+								"queries-bjq-fq.txt",
+								"q=text_all:(%s)"
+										+ "&fl=id,score&sort=score desc&fq={!join from=join_id to=id}acl:%s"),
+
+						new QueryFile("queries-bjq.txt",
+								"q=text_all:(%s) AND _query_:\"{!join from=join_id to=id}acl:%s\""
+										+ "&fl=id,score&sort=score desc"),
+						new QueryFile("queries-no-acls.txt",
+								"q=text_all:(%s)&fl=id,score&sort=score desc"));
+		
+		QueryGenerator qg = new QueryGenerator();
+		qg.queryConsumer = new QGen() {
+			@Override
+			public void onQuery(String terms, String aclRange) {
+				for (QGen consumer : consumers) {
+					consumer.onQuery(terms, aclRange);
+				}
+			}
+		};
+		
+		main(qg);
+		
+		for (QueryFile consumer : consumers) {
+			consumer.close();
+		}
+	}
 
 protected static void main(QueryGenerator qg) {
 	try {
@@ -26,30 +92,20 @@ protected static void main(QueryGenerator qg) {
 }
 
   void generateQueries() throws Exception {
-    BufferedWriter writer = new BufferedWriter(new FileWriter(OUTPUT_FILE));
     for (int idx = 0; idx < 10000; ++idx) {
       // Solrmeter doesn't need the above in the text file, you enter that in a screen.
       String terms = getTerms(idx);
 	String aclRange = getAclRange(idx);
 	String place = getPlace(idx);
-	String raw = query(terms, aclRange, place);
 //      String raw = String.format("q=kind:instance OR text_all:(%s)" +
 //          "&fl=id,score&sort=score desc&fq={!bbox}&sfield=place&pt=%s&d=100",
 //          getTerms(idx), getPlace(idx));
 
 //      String uri = URLEncoder.encode(raw, "UTF-8");
-      writer.write(raw);
-      writer.newLine();
+	queryConsumer.onQuery(terms, aclRange);
     }
-    writer.close();
   }
 
-protected String query(String terms, String aclRange, String place) {
-	return String.format("q=text_all:(%s)" +
-			"&fl=id,score&sort=score desc&fq={!join from=join_id to=id}acl:%s",
-          //"&fl=id,score&sort=score desc&fq={!join from=few_join_id to=few_id}acl:%s",
-          terms, aclRange, place);
-}
   String getTerms(int idx) {
     // Idea here is that if idx mod 10 is between 1-5 (inclusive), use one term. 6-7 two terms, 8-9 three terms. ORed.
     int modulus = idx % 10;
